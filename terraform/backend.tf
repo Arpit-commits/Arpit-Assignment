4ef68c927d97df352643edc74be2c78d1ac1e2eb
EC2 Default User <ec2-user@ip-172-31-79-92.ec2.internal>
2025-02-02 19:26:40 +0000

    Update

--- terraform/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "acs730-169572237-tfstate"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-  }
-}
--- terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "ec2_public_ip" {
-  value = aws_instance.web_instance.public_ip
-}
-
-output "ecr_repo_url" {
-  value = aws_ecr_repository.ecr_repo.repository_url
-}
-
-
